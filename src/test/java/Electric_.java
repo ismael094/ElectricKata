d74c749584da951bf85431692f413707a86b0a78
ismael094 <ismaelpitufo@gmail.com>
2019-12-17 09:53:39 +0000

    Four test

--- src/test/java/Electric_.java
+++ src/test/java/Electric_.java
@@ -37,14 +37,33 @@ public class Electric_ {
         assertThat(checkDates(receipts)).isEqualTo("Gap");
     }
 
+    @Test
+    public void with_two_receipt_with_overlap_should_return_overlap() {
+        Receipt receipt = new Receipt(LocalDate.of(2019, Month.SEPTEMBER, 17),LocalDate.of(2019, Month.OCTOBER, 17));
+        Receipt receipt2 = new Receipt(LocalDate.of(2019, Month.SEPTEMBER, 17),LocalDate.of(2019, Month.DECEMBER, 17));
+        List<Receipt> receipts = new ArrayList<>();
+        receipts.add(receipt);
+        receipts.add(receipt2);
+        assertThat(checkDates(receipts)).isEqualTo("Overlap");
+    }
+
     private String checkDates(List<Receipt> receipts) {
         if (receipts.size() <= 1)
             return "Good";
         for (int i = 0; i < receipts.size() -1; i++) {
+
             if (gap(receipts.get(i).getFinish(),receipts.get(i+1).getStart()))
                 return "Gap";
+            if (overlap(receipts.get(i).getFinish(),receipts.get(i+1).getStart()))
+                return "Overlap";
         }
         return "Good";
+    }
+
+    private boolean overlap(LocalDate finishDate, LocalDate startDate) {
+        if ( ChronoUnit.MONTHS.between(finishDate, startDate) < 0)
+            return true;
+        return false;
     }
 
     private boolean gap(LocalDate finishDate, LocalDate startDate) {
@@ -68,6 +87,15 @@ public class Electric_ {
 
         public LocalDate getFinish() {
             return finish;
+        }
+    }
+
+    class User{
+        private List<Receipt> receipts;
+        private String name;
+
+        public User(List<Receipt> receipts) {
+            this.receipts = receipts;
         }
     }
 }
