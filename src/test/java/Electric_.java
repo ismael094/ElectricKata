30caf8f06eddda770d07975387a72a9c1876d86e
ismael094 <ismaelpitufo@gmail.com>
2019-12-17 09:43:00 +0000

    Third test

--- src/test/java/Electric_.java
+++ src/test/java/Electric_.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,10 +27,30 @@ public class Electric_ {
         assertThat(checkDates(receipts)).isEqualTo("Good");
     }
 
+    @Test
+    public void with_two_receipt_with_gap_should_return_gap() {
+        Receipt receipt = new Receipt(LocalDate.of(2019, Month.SEPTEMBER, 17),LocalDate.of(2019, Month.OCTOBER, 17));
+        Receipt receipt2 = new Receipt(LocalDate.of(2019, Month.NOVEMBER, 17),LocalDate.of(2019, Month.DECEMBER, 17));
+        List<Receipt> receipts = new ArrayList<>();
+        receipts.add(receipt);
+        receipts.add(receipt2);
+        assertThat(checkDates(receipts)).isEqualTo("Gap");
+    }
+
     private String checkDates(List<Receipt> receipts) {
-        if (receipts.size() <= 2)
+        if (receipts.size() <= 1)
             return "Good";
-        return "";
+        for (int i = 0; i < receipts.size() -1; i++) {
+            if (gap(receipts.get(i).getFinish(),receipts.get(i+1).getStart()))
+                return "Gap";
+        }
+        return "Good";
+    }
+
+    private boolean gap(LocalDate finishDate, LocalDate startDate) {
+        if ( ChronoUnit.MONTHS.between(finishDate, startDate) >= 1)
+            return true;
+        return false;
     }
 
     class Receipt {
